ed00708bd447d411da82aef0ef73831ec9c4e249
EMMUUU28 <francisgudinho1234@gmail.com>
2025-06-12 15:56:57 +0530

    [Emmanuel] Deleted Files (trash)

--- xlsx_processor1/forecast/views.py
+++ xlsx_processor1/forecast/views.py
@@ -463,7 +463,7 @@ class ForecastNoteViewSet(viewsets.ModelViewSet):
 
         return queryset
 
-
+# Done 
 class FileCategoryDownloadAPIView(APIView):
     def get(self, request):
         """
@@ -557,7 +557,7 @@ class FileCategoryDownloadAPIView(APIView):
                 response['X-Missing-Categories'] = message
             return response
     
-
+# Done
 class SheetUploadViewSet(viewsets.ModelViewSet):
     queryset = SheetUpload.objects.all()
     serializer_class = SheetUploadSerializer
