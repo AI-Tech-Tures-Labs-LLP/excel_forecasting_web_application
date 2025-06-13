115c7fca4812ac4b7abffd69344d6598c319fa52
EMMUUU28 <francisgudinho1234@gmail.com>
2025-06-13 16:14:23 +0530

    [Emmanuel] Added Auth APIs

--- xlsx_processor1/authentication/serializers.py
+++ xlsx_processor1/authentication/serializers.py
@@ -1,13 +1,24 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import Role
 
 User = get_user_model()
 
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = ['id', 'name']
+
 class CustomUserSerializer(serializers.ModelSerializer):
+    role = RoleSerializer(read_only=True)
+    role_id = serializers.PrimaryKeyRelatedField(
+        queryset=Role.objects.all(), source='role', write_only=True, required=False
+    )
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'role']
+        fields = ['id', 'username', 'email', 'role', 'role_id']
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
